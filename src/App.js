551ddcb27014e2ba6690b8c7201b2c804a67615e
iamraselmolla <raselmolla6336@gmail.com>
2023-11-05 23:56:54 +0600

    setup project, added menu and courses json

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect } from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Main from './componenets/Main';
+import Home from './componenets/home/Home';
 
 function App() {
+  useEffect(() => {
+    fetch('courses.json')
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err))
+  }, [])
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        }
+      ]
+    }
+  ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    < div className="App" >
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
